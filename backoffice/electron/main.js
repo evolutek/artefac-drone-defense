c9db64b21cce43b6accabd6d9f870ea134ffa42f
Dimitri Almon <dimitri.almon@gmail.com>
2025-11-26 09:27:00 +0100

    Adding desktop frnt

---
+++ backoffice/electron/main.js
@@ -0,0 +1,23 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 760,
+    webPreferences: { nodeIntegration: false, contextIsolation: true }
+  })
+  const devUrl = process.env.DEV_URL
+  const url = devUrl || `file://${path.join(__dirname, '../index.html')}`
+  win.loadURL(url)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow() })
+})
+
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit() })
